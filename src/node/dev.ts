8c850c3fb21d017de71dbbdbce95fae8d8e2bfcd
fanyinqian <fanyinqian@meituan.com>
2024-06-03 19:53:11 +0800

    feat: mvp-02

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,10 @@
 import { createServer } from "vite";
+import { pluginIndexHtml } from "./plugin-siland/indexHtml";
+import pluginReact from "@vitejs/plugin-react";
 
 export async function createDevServer(root:string) {
   return createServer({
     root,
+    plugins: [pluginIndexHtml(), pluginReact()],
   });
 }
